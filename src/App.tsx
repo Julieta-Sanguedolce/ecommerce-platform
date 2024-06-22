94be983b0900c7fa0dc56594490a0e1f1b9729c7
“Jaseera <j.p.mohamedabubacker@gmail.com>
2024-06-22 14:26:35 +0100

    Refactor and package files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { HeaderComponent } from "./components/Header/HeaderComponent";
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <HeaderComponent />
+            </header>
+        </div>
+    );
+}
+
+export default App;
